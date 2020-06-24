6d9272e818f7eb19dc4826b248435d1dc0b4d2c8
Dimas <dimasriat@gmail.com>
2020-06-25 02:26:11 +0700

    crud mongoose udah bisaa

--- routers/index.js
+++ routers/index.js
@@ -7,14 +7,30 @@ router.get("/", (req, res) => {
 	res.render("index", { nama: "Dimas Riatmodjo" });
 });
 
-router.get("/add", (req, res) => {
-	User.create(
-		{ username: "dimasriat", password: "4ice3skrim5troberi" },
-		(err) => {
-			if (err) throw err;
-			res.send("item berhasil dibuat");
-		}
-	);
+router.get("/create", (req, res) => {
+	res.render("create");
+});
+
+router.post("/create", (req, res) => {
+	const data = { username: req.body.username, password: req.body.password };
+	User.create(data, (err) => {
+		if (err) throw err;
+		res.redirect("/read");
+	});
+});
+
+router.get("/read", (req, res) => {
+	User.find((err, results) => {
+		if (err) throw err;
+		res.render("read", { results: results });
+	});
+});
+
+router.get("/delete/:id", (req, res) => {
+	User.findByIdAndDelete(req.params.id, (err) => {
+		if (err) throw err;
+		res.redirect("/read");
+	});
 });
 
 module.exports = router;
